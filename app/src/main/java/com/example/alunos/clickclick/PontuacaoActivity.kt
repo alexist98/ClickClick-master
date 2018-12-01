80df9829489a5fe24822ba75da3a90482184e3ec
alexist98 <alexistoigo@gmail.com>
2018-12-01 21:24:50 -0200

    Adicionado ranking top 3

--- app/src/main/java/com/example/alunos/clickclick/PontuacaoActivity.kt
+++ app/src/main/java/com/example/alunos/clickclick/PontuacaoActivity.kt
@@ -13,18 +13,31 @@ class PontuacaoActivity : AppCompatActivity() {
     internal lateinit var jogarNovamente: Button
     internal lateinit var gameScoreTextView2: TextView
     var score = MainActivity.score1.toString()
+    /*var top1 = 0
+    var top2 = 0
+    var top3 = 0*/
+    companion object {
+        var top1 = 0
+        var top2 = 0
+        var top3 = 0
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val numbers: MutableList<Int> = mutableListOf(top1, top2, top3)
+        val readOnlyView: List<Int> = numbers
         setContentView(R.layout.activity_pontuacao)
 
         jogarNovamente = findViewById<Button>(R.id.jogar_novamente)
         gameScoreTextView2 = findViewById<TextView>(R.id.game_score_text_view2)
+        gameScoreTextView2.text = getString(R.string.sua_pontuacao, score)
 
-
-        gameScoreTextView2.text = getString(R.string.sua_pontuacao, score.toString())
-
+        numbers.add(score.toInt())
+        val ordenado = numbers.sortedWith((reverseOrder()))
+        top1=ordenado.component1()
+        top2=ordenado.component2()
+        top3=ordenado.component3()
     }
 
 
--- app/src/main/java/com/example/alunos/clickclick/RecordActivity.kt
+++ app/src/main/java/com/example/alunos/clickclick/RecordActivity.kt
@@ -2,11 +2,35 @@ package com.example.alunos.clickclick
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RecordActivity : AppCompatActivity() {
+
+    internal lateinit var top1TextView: TextView
+    internal lateinit var top2TextView: TextView
+    internal lateinit var top3TextView: TextView
+
+
+    var top1 = PontuacaoActivity.top1.toString()
+    var top2 = PontuacaoActivity.top2.toString()
+    var top3 = PontuacaoActivity.top3.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
+
+
+        top1TextView = findViewById<TextView>(R.id.top1_text_view)
+        top1TextView.text = getString(R.string.sua_pontuacao, top1.toString())
+
+        top2TextView = findViewById<TextView>(R.id.top2_text_view)
+        top2TextView.text = getString(R.string.sua_pontuacao, top2.toString())
+
+        top3TextView = findViewById<TextView>(R.id.top3_text_view)
+        top3TextView.text = getString(R.string.sua_pontuacao, top3.toString())
     }
+
+
+
+
 }

8b1129b9f9eca32b4e1c0d228c5d0cb986e630d6
alexist98 <alexistoigo@gmail.com>
2018-12-01 16:28:28 -0200

    Algumas att

--- app/src/main/java/com/example/alunos/clickclick/AboutActivity.kt
+++ app/src/main/java/com/example/alunos/clickclick/AboutActivity.kt
@@ -6,11 +6,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.Window
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_about)
     }
 
